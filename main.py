0296e6f8d16737557fcbaf0d99627ce1d47de20d
Amy Xu <xuamyj@gmail.com>
2015-08-06 03:11:03 -0700

    still experimenting

--- main.py
+++ main.py
@@ -17,16 +17,29 @@
 # Citations: Udacity's Web Development class taught by Steve Huffman
 
 import os
+import re
+import random
+import hashlib
+import hmac
+import logging
+import json
+from string import letters
+from userdb import User
 
 import webapp2
 import jinja2
 
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
 secret = 'thankyoustevehuffman'
+
+# General utilities
+# -----------------
+# Rendering templates, setting cookies, user authentication
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
@@ -83,10 +96,114 @@ class BlogHandler(webapp2.RequestHandler):
         else:
             self.format = 'html'
 
+
+# New account utilities
+# ---------------------
+# Username/password email validators
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Signup(BlogHandler):
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username = self.username,
+                      email = self.email)
+
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
+
+
+# HTTP server things
+# ------------------
+
 class MainPage(BlogHandler):
     def get(self):
         self.write('Hello, what is my life...!')
 
+class Register(Signup):
+    def done(self):
+        #make sure the user doesn't already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('signup-form.html', error_username = msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/welcome')
+
+class Login(BlogHandler):
+    def get(self):
+        self.render('login-form.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        u = User.login(username, password)
+        if u:
+            self.login(u)
+            self.redirect('/welcome')
+        else:
+            msg = 'Invalid login'
+            self.render('login-form.html', error = msg)
+
+class Logout(BlogHandler):
+    def get(self):
+        self.logout()
+        self.redirect('/signup')
+
+class Welcome(BlogHandler):
+    def get(self):
+        if self.user:
+            self.render('welcome.html', username = self.user.name)
+        else:
+            self.redirect('/signup')
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/signup', Register),
+    ('/login', Login),
+    ('/logout', Logout),
+    ('/welcome', Welcome),
 ], debug=True)
